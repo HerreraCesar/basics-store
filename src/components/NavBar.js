9a3d9a4da6fe2e41c40c9485abd97e6fb2eb5ab9
César Herrera <herrera.cesar.arg@outlook.com>
2021-12-14 21:22:24 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className='NavBar'>
+            <div className='logo'>
+                <span>E-COMMERCE HERRERA</span>
+            </div>
+            <div className='links'>
+                <a href='#'>Inicio</a>
+                <a href='#'>Productos</a>
+                <a href='#'>Contacto</a>
+                <a href='#'>Mi cuenta</a>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
